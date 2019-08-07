426d9efb5e8e52e4d1a7e31e2eb1624a122b1b19
gbwalker <gabrielbwalker@gmail.com>
2019-08-06 23:53:56 -0400

    More cleaning in progress.

--- process_data.py
+++ process_data.py
@@ -2,7 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from datetime import datetime
 import re
+import requests
+from bs4 import BeautifulSoup
 
 # Make dataframes appear as full tables, not wrapped.
 
@@ -26,7 +29,7 @@ raw = pd.read_csv('C:/Users/Gabriel/Desktop/FEC individual contributions/2019-20
 # Fix the column names to something more legible.
 # See the variable descriptions here: https://www.fec.gov/campaign-finance-data/contributions-individuals-file-description/.
 
-raw.columns = ['recipient', 'amendment', 'report', 'election', 'image', 'type', 'entity', 'name', 'city', 'state', 'zip',
+raw.columns = ['id_committee', 'amendment', 'report', 'election', 'image', 'type', 'entity', 'name', 'city', 'state', 'zip',
                'employer', 'occupation', 'date', 'amount', 'id_other', 'id_transaction', 'id_report', 'memo_code', 'memo_text', 'fec_record']
 
 ### Process the data.
@@ -58,7 +61,7 @@ del df['name']
 # Remove other variables that don't add any predictive value, such as the image number, transaction ID, and FEC record (and possibly memo code).
 # Delete 22Y entries, which are refunded donations?
 
-del df['image'], df['id_transaction'], df['fec_record']
+del df['image'], df['id_transaction'], df['fec_record'], df['report']
 
 ## Split the zip code.
 
@@ -66,7 +69,7 @@ zips = df['zip']
 
 # Make a separate list of just the very long zip codes.
 
-zips_long = zips[zips.dropna().str.len() > 5]
+zips_long = zips.dropna()[zips.dropna().str.len() > 5]
 
 # Identify the first and second parts of the zip codes.
 # Append .loc[:,0] to turn them into Series.
@@ -85,8 +88,67 @@ zips_final.columns = ['zip', 'zip2']
 del df['zip']
 df = df.join(zips_final)
 
+### Format the date column.
 
+# This function takes in a string argument of the format that appears in the FEC data.
+# It returns a datetime object.
+
+def string_to_date(s):
+
+    # Convert the integer to a string.
+
+    s = str(s)
+
+    # The year is always the last four digits.
+
+    year = re.search(r'(\d\d\d\d$)', s)[0]
+
+    # If there are two digits for the month and day.
+
+    if len(s) == 8:
+        month = s[0:2]
+        day = s[2:4]
+
+    # If there's only one digit for the month or day.
+
+    if len(s) == 7:
+        if s[0] in ['2', '3'] or s[1] == '3' or s[2] == '0':
+            month = s[0]
+            day = s[1:3]
+        else:
+            month = s[0:2]
+            day = s[2]
+
+    # If there's only one digit for the month AND day.
+    
+    if len(s) == 6:
+        month = s[0]
+        day = s[1]
+    
+    date = month + '-' + day + '-' + year
+
+    return(datetime.strptime(date, '%m-%d-%Y').date())
+
+# Replace the date column in the original dataframe.
+
+df = df.assign(date = df.date.apply(string_to_date))
+
+### Make some variable values more legible with full words.
+# All according to the chart here: https://www.fec.gov/campaign-finance-data/contributions-individuals-file-description/.
+
+amendments = {'N':'New', 'A':'Amendment', 'T':'Termination'}
+elections = {'P':'Primary', 'G':'General', 'O':'Other', 'C':'Convention', 'R':'Runoff', 'S':'Special', 'E':'Recount'}
+
+df['amendment'] = df['amendment'].map(amendments)
+df['election'] = df['election'].map(elections)
+
+### Scrape the FEC websites for more mapping pairs.
+
+
+
+##############################
 ### Import the committee data.
+##############################
 
 # Read in the committee data.
 
@@ -95,10 +157,23 @@ raw_committee = pd.read_csv(
 
 # Add a new header based on https://www.fec.gov/campaign-finance-data/committee-master-file-description/.
 
-raw_committee.columns = ['recipient', 'name', 'treasurer', 'street1', 'street2', 'city', 'state',
+raw_committee.columns = ['id_committee', 'name', 'treasurer', 'street1', 'street2', 'city', 'state',
                          'zip', 'designation', 'type', 'party', 'frequency', 'category', 'connection', 'id_candidate']
 
-# Copy only the committee information of interest and match the names in the original dataframe.
+# Copy only the committee information of interest.
+
+df_committee = raw_committee[['id_committee', 'name', 'designation', 'type', 'party', 'frequency', 'category', 'connection', 'id_candidate']]
+
+# Join the committee info with the original donation data.
+
+df = pd.merge(df, df_committee, how='left', on='id_committee', suffixes=['_donation', '_committee'])
+
+##############################
+### Import the candidate data.
+##############################
+
+
+
 
 # Count how many give outside their state.
 
@@ -106,7 +181,7 @@ raw_committee.columns = ['recipient', 'name', 'treasurer', 'street1', 'street2',
 
 # Find candidate election results. Manipulate the date to determine how early/late donation is.
 
-# https: // www.fec.gov/data/receipts /?data_type = processed & two_year_transaction_period = 2020 & min_date = 01 % 2F01 % 2F2019 & max_date = 12 % 2F31 % 2F2020
+
 # https: // www.fec.gov/campaign-finance-data/contributions-individuals-file-description/
 # https://www.fec.gov/campaign-finance-data/committee-master-file-description/
 # https: // github.com/clintval/gender-predictor
